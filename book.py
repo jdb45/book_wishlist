0bb56ebdc92bd04871e5e35b24e4f57d77308281
Jeremy <acnajcadcac.yahoo.com>
2017-01-24 10:56:50 -0600

    Added N/A to the date read when the book has not been read yet

--- book.py
+++ book.py
@@ -19,15 +19,17 @@ class Book:
 
     def __str__(self):
         read_str = 'no'
+        date_read_str = 'N/A'
         if self.read:
             read_str = 'yes'
+            date_read_str = self.dateRead
 
         id_str = self.id
         if id == -1:
             id_str = '(no id)'
 
         template = 'id: {} Title: {} Author: {} Read: {} Date Read: {}'
-        return template.format(id_str, self.title, self.author, read_str, self.dateread)
+        return template.format(id_str, self.title, self.author, read_str, date_read_str)
 
 
     def __eq__(self, other):
